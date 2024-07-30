b0de339d7d06560a1d4f1f1de4304864afde2e7d
ksheyon123 <kshyeon123@gmail.com>
2024-07-30 17:56:25 +0900

    :sparkles:feat before applying lots of specs

--- src/app/physics/drop-mass/page.tsx
+++ src/app/physics/drop-mass/page.tsx
@@ -11,7 +11,8 @@ import { useMesh } from "@/hooks/useMesh";
 const Page = () => {
   const { createCamera } = useCamera();
   const { createRenderer, createScene } = useRenederer();
-  const { testor } = useMesh();
+  const { createMesh, calForce, calAcceleration, calVelocity, calCoordinate } =
+    useMesh();
 
   const canvasRef = useRef<HTMLDivElement>();
 
@@ -25,20 +26,48 @@ const Page = () => {
 
   useEffect(() => {
     if (isMounted) {
+      console.log("IS LOADED");
       const scene = createScene();
       const camera = createCamera();
+      camera.position.set(0, 0, 200);
+      camera.lookAt(0, 0, 0);
       const renderer = createRenderer();
       canvasRef.current && canvasRef.current.appendChild(renderer.domElement);
+      const mesh = createMesh();
+      mesh.position.set(0, 100, 0);
+      scene.add(mesh);
 
+      const vel = new THREE.Vector3();
       let id: any;
       const animate = () => {
         id = requestAnimationFrame(animate);
+        const prevPosition = mesh.position.clone();
+        const force = calForce(new THREE.Vector3(), prevPosition);
+        const acc = calAcceleration(force);
+        const newVel = calVelocity(vel, acc);
+        const newP = calCoordinate(prevPosition, newVel);
+        if (newP.y <= 0) {
+          newP.set(0, 0, 0);
+          newVel.set(0, 0, 0);
+        }
+        mesh.position.set(newP.x, newP.y, newP.z);
+        vel.set(newVel.x, newVel.y, newVel.z);
         renderer.render(scene, camera);
       };
-      return () => cancelAnimationFrame(id);
+
+      //   let d = setInterval(() => {
+      //     clearInterval(d);
+      //     cancelAnimationFrame(id);
+      //     animate();
+      //   }, 5000);
+      animate();
+
+      return () => {
+        cancelAnimationFrame(id);
+        // clearInterval(d);
+      };
     }
-    testor();
-  }, []);
+  }, [isMounted]);
 
   return <div ref={canvasRef as RefObject<HTMLDivElement>} />;
 };
--- src/hooks/useMesh.ts
+++ src/hooks/useMesh.ts
@@ -1,18 +1,30 @@
 import * as THREE from "three";
 
 export const useMesh = () => {
-  const dt = 1 / 60; // [1/s]
-  const gravity = 9.8; // [m/s**2]
-  const mass = 5; // [kg]
+  const dt = 1 / 6; // [s] (10 times faster)
+  const g = -9.81; // [m/s**2]
+  const mass = 1; // [kg]
+
+  /**
+   *
+   * @returns {THREE.Mesh} Mesh Object
+   */
   const createMesh = () => {
-    const geometry = new THREE.SphereGeometry(15, 32, 16);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-    const sphere = new THREE.Mesh(geometry, material);
-    return sphere;
+    const geometry = new THREE.BoxGeometry(5, 5, 5);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const cube = new THREE.Mesh(geometry, material);
+    return cube;
   };
 
+  /**
+   * Calculates the force based on given force, and add gravity acceleration.
+   *
+   * @param {THREE.Vector3} force - The force vector [N].
+   * @param {THREE.Vector3} curPosition - The current position vector [m].
+   * @returns {THREE.Vector3}
+   */
   const calForce = (force: THREE.Vector3, curPosition: THREE.Vector3) => {
-    const gravity = new THREE.Vector3(0, -9.81 * mass, 0);
+    const gravity = new THREE.Vector3(0, g, 0);
     const totalForce = force.clone().add(gravity);
     if (curPosition.y <= 0 && force.y < 0) {
       totalForce.y = 0;
@@ -24,14 +36,10 @@ export const useMesh = () => {
    * Calculates the acceleration based on the given force and mass.
    *
    * @param {THREE.Vector3} force - The force vector [N].
-   * @param {number} mass - The mass of the object [kg].
    * @returns {THREE.Vector3} The acceleration vector [m/s^2].
    */
-  const calAcceleration = (force: THREE.Vector3, m = mass) => {
-    if (m === 0) {
-      throw new Error("Mass cannot be zero.");
-    }
-    const acceleration = force.clone();
+  const calAcceleration = (force: THREE.Vector3) => {
+    const acceleration = force.clone().multiplyScalar(dt);
     return acceleration;
   };
 
@@ -58,26 +66,53 @@ export const useMesh = () => {
    */
   const calCoordinate = (prevPosition: THREE.Vector3, vel: THREE.Vector3) => {
     const cVel = vel.clone();
-    const newPosition = prevPosition.add(cVel.multiplyScalar(dt));
+    const cPosition = prevPosition.clone();
+    const newPosition = cPosition.add(cVel.multiplyScalar(dt));
     return newPosition;
   };
 
-  const testor = () => {
-    const force = new THREE.Vector3();
-    const initVelocity = new THREE.Vector3();
-    let initPosition = new THREE.Vector3(0, 10, 0);
+  const collide = (
+    mesh: THREE.Mesh,
+    prevPosition: THREE.Vector3,
+    direction: THREE.Vector3
+  ) => {
+    let collidableMeshList: any[] = [];
+    for (
+      var vertexIndex = 0;
+      vertexIndex < mesh.geometry.attributes.position.array.length;
+      vertexIndex++
+    ) {
+      var localVertex = new THREE.Vector3()
+        .fromBufferAttribute(mesh.geometry.attributes.position, vertexIndex)
+        .clone();
+      var globalVertex = localVertex.applyMatrix4(mesh.matrix);
+      var directionVector = globalVertex.sub(mesh.position);
 
-    for (let i = 0; i < 120; i++) {
-      const newForce = calForce(force, initPosition);
-      const newAcceleration = calAcceleration(newForce);
-      const newVelocity = calVelocity(initVelocity, newAcceleration);
-      const newCoord = calCoordinate(initPosition, newVelocity);
-      initPosition = newCoord;
-      if (i === 119) {
-        console.log(initPosition);
+      var ray = new THREE.Raycaster(
+        mesh.position,
+        directionVector.clone().normalize()
+      );
+      var collisionResults = ray.intersectObjects(collidableMeshList);
+      if (
+        collisionResults.length > 0 &&
+        collisionResults[0].distance < directionVector.length()
+      ) {
+        // a collision occurred... do something...
       }
     }
+    return new THREE.Vector3();
   };
+
+  // pe = mgh
+  const potential = () => {};
+
+  // v ** 2 - v0 ** 2 = 2gh
+  const kinetic = () => {};
+
+  // Drag Coefficient = 0.47
+  // ρ (Air density) = 1.225 [kg / m3]
+  //mg= 1/2 * Cd * ρ * A * v ** 2 > 종단 속도
+  const terminalVelocity = () => {};
 
   return {
     createMesh,
@@ -86,7 +121,5 @@ export const useMesh = () => {
     calAcceleration,
     calVelocity,
     calCoordinate,
-
-    testor,
   };
 };
