6b7e65639f87c31fcdc5b546fbf493f12c9bd13a
ksheyon123 <kshyeon123@gmail.com>
2024-09-19 17:52:19 +0900

    :sparkles:feat add friction

---
+++ src/app/physics/friction/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { RefObject, useEffect, useRef, useState } from "react";
+
+import * as THREE from "three";
+
+import { useCamera } from "@/hooks/useCamera";
+import { useRenederer } from "@/hooks/useRenderer";
+import { useMesh } from "@/hooks/useMesh";
+import styled from "styled-components";
+import { ForwardedCanvas } from "@/components/Canvas/Canvas";
+import { makeMesh } from "@/utils/threejs.utils";
+
+const Page = () => {
+  const { createCamera } = useCamera();
+  const { createRenderer, createScene } = useRenederer();
+  const {} = useMesh();
+
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  const hz = 1 / 60; //seconds
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      console.log("IS LOADED");
+      const scene = createScene();
+      const camera = createCamera();
+      camera.position.set(0, 0, 10);
+      camera.lookAt(0, 0, 0);
+      const renderer = createRenderer(
+        window.innerWidth,
+        window.innerHeight - 80
+      );
+      canvasRef.current && canvasRef.current.appendChild(renderer.domElement);
+
+      let id: any;
+      const animate = () => {
+        id = requestAnimationFrame(animate);
+      };
+
+      const plane = makeMesh(10, 10, 0.1, 0x000000);
+      scene.add(plane);
+
+      animate();
+
+      return () => {
+        cancelAnimationFrame(id);
+        renderer.render(scene, camera);
+      };
+    }
+  }, [isMounted]);
+
+  return (
+    <ForwardedCanvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight - 80}
+    />
+  );
+};
+
+export default Page;
