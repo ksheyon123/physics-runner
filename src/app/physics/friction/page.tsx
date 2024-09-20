e5fa946ee8b7c9050b1b2f7c57c4281d309035ed
ksheyon123 <kshyeon123@gmail.com>
2024-09-20 16:05:23 +0900

    :sparkles:feat colour intersects

--- src/app/physics/friction/page.tsx
+++ src/app/physics/friction/page.tsx
@@ -11,35 +11,51 @@ import { useMesh } from "@/hooks/useMesh";
 import styled from "styled-components";
 import { ForwardedCanvas } from "@/components/Canvas/Canvas";
 import { bindMesh, makeMesh, makeSphere } from "@/utils/threejs.utils";
+import { useRaycaster } from "@/hooks/useRaycaster";
 
 const Page = () => {
+  const gap = 80;
+  const canvasWidth = window.innerWidth;
+  const canvasHeight = window.innerHeight - gap;
   const { createCamera } = useCamera();
   const { createRenderer, createScene } = useRenederer();
   const {} = useMesh();
+  const { rayCrossing, calPointerCoord } = useRaycaster(
+    canvasWidth,
+    canvasHeight,
+    gap
+  );
 
   const canvasRef = useRef<HTMLDivElement>(null);
+
+  const sceneRef = useRef<any>(null);
+  const cameraRef = useRef<any>(null);
+  const rendererRef = useRef<any>(null);
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
   const hz = 1 / 60; //seconds
 
   useEffect(() => {
+    sceneRef.current = createScene();
+    cameraRef.current = createCamera();
+    cameraRef.current.position.set(0, 0, 10);
+    rendererRef.current = createRenderer(canvasWidth, canvasHeight);
     setIsMounted(true);
   }, []);
 
   useEffect(() => {
     if (isMounted) {
       console.log("IS LOADED");
-      const scene = createScene();
-      const camera = createCamera();
-      camera.position.set(0, 0, 10);
+      const camera = cameraRef.current;
+      const renderer = rendererRef.current;
+      const scene = sceneRef.current;
       //   camera.lookAt(0, 0, 0);
-      const renderer = createRenderer(
-        window.innerWidth,
-        window.innerHeight - 80
-      );
       canvasRef.current && canvasRef.current.appendChild(renderer.domElement);
       const controls = new OrbitControls(camera, renderer.domElement);
+
+      const dot = makeMesh(0.1, 0.1, 0.1, 0x000000);
+      scene.add(dot);
 
       const plane = makeMesh(10, 10, 1, 0x000000);
       plane.position.set(0, -5, 0);
@@ -48,9 +64,10 @@ const Page = () => {
 
       const bar = makeMesh(0.1, 10, 0.1, 0x00ff00);
       const sphere1 = makeSphere(0.2, 32);
+      sphere1.name = "ball";
       sphere1.position.set(0, 5, 0);
       const sphere2 = makeSphere(0.2, 32);
-
+      sphere2.name = "ball";
       const merged = bindMesh([bar, sphere1, sphere2]);
 
       scene.add(merged);
@@ -70,11 +87,35 @@ const Page = () => {
     }
   }, [isMounted]);
 
+  useEffect(() => {
+    if (isMounted) {
+      canvasRef.current!.addEventListener("mousemove", (e) => {
+        const { x, y } = calPointerCoord(e);
+        const intersects = rayCrossing(
+          { x, y },
+          cameraRef.current,
+          sceneRef.current
+          // "ball"
+        );
+        for (let i = 0; i < intersects.length; i++) {
+          intersects[i].object!.material.color.set(0x000000);
+        }
+
+        rendererRef.current.render(sceneRef.current, cameraRef.current);
+      });
+      return () => {
+        canvasRef.current!.removeEventListener("mousemove", (e) => {
+          calPointerCoord(e);
+        });
+      };
+    }
+  }, [isMounted]);
+
   return (
     <ForwardedCanvas
       ref={canvasRef}
-      width={window.innerWidth}
-      height={window.innerHeight - 80}
+      width={canvasWidth}
+      height={canvasHeight}
     />
   );
 };
--- src/hooks/useRaycaster.ts
+++ src/hooks/useRaycaster.ts
@@ -1,12 +1,39 @@
 import * as THREE from "three";
 
-const useRaycaster = () => {
-  const rayCrossing = (event: any) => {
+export const useRaycaster = (width: number, height: number, gap: number) => {
+  const calPointerCoord = (event: any) => {
+    const x = (event.clientX / width) * 2 - 1;
+    const y = -((event.clientY - gap) / height) * 2 + 1;
+    return {
+      x,
+      y,
+    };
+  };
+
+  const rayCrossing = (
+    coord: any,
+    camera: THREE.PerspectiveCamera,
+    scene: THREE.Scene,
+    filter?: string
+  ) => {
     // calculate pointer position in normalized device coordinates
     // (-1 to +1) for both components
-
-    const x = (event.clientX / window.innerWidth) * 2 - 1;
-    const y = -(event.clientY / window.innerHeight) * 2 + 1;
+    const pointer = new THREE.Vector2(coord.x, coord.y);
     const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(pointer, camera);
+    let objs = scene.children;
+    console.log(objs);
+    if (filter) {
+      objs = scene.children.filter((el) => el.name === filter);
+    }
+
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects(objs);
+    return intersects;
+  };
+
+  return {
+    calPointerCoord,
+    rayCrossing,
   };
 };
